d223da73b3806f15a34c4d8e3ac13af7e3aaadfb
Alison Yoon <aliyooncreative@gmail.com>
2018-03-23 18:36:43 +0900

    minesweeper with array iterators

--- minesweeper/src/minesweeper.js
+++ minesweeper/src/minesweeper.js
@@ -1,14 +1,41 @@
-let board = [[' ',' ',' '],[' ',' ',' '],[' ',' ',' ']];
-const printBoard = (board) => {
-  console.log('Current Board:');
-  console.log(board[0].join(' | '));
-  console.log(board[1].join(' | '));
-  console.log(board[2].join(' | '));
+const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
+  let board = [];
+  for(let rows = 0 ; rows < numberOfRows ; rows++) {
+    let row = [];
+    for(let columns = 0 ; columns < numberOfColumns ; columns++) {
+      row.push(' ');
+    }
+    board.push(row);
+  }
+  return board;
 };
 
+const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
+  let board = [];
+  for(let rows = 0 ; rows < numberOfRows ; rows++) {
+    let row = [];
+    for(let columns = 0 ; columns < numberOfColumns ; columns++) {
+      row.push(null);
+    }
+    board.push(row);
+  }
+  let numberOfBombsPlaced = 0;
+  while(numberOfBombsPlaced < numberOfBombs){
+    let randomRowIndex = Math.floor(Math.random() * numberOfRows);
+    let randomColumnIndex = Math.floor(Math.random() * numberOfColumns);
+    board[randomRowIndex][randomColumnIndex] = 'B';
+    numberOfBombsPlaced++;
+    //I have to learn control flow to prevent placing bombs on top of already existing bombs
+  }
+  return board;
+};
 
+const printBoard = board => {
+  console.log(board.map(row => row.join(' | ')).join('\n'));
+}
 
-printBoard(board);
-board[0][1] = '1';
-board[2][2] = 'B';
-printBoard(board);
+let playerBoard = generatePlayerBoard(3,4);
+let bombBoard = generateBombBoard(3,4,5);
+
+console.log('Player Board: ' + printBoard(playerBoard));
+console.log('Bomb Board: ' + printBoard(bombBoard));
